bd6756b33939c58bfc3550feff54791b022cfbf9
jaiv shah <jaiv.maurya@gmail.com>
2021-05-02 18:40:41 -0400

    Build clone

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
+import NewsFeed from "./Newsfeed";
+import Stats from "./Stats";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <div className="app__header">
         <Header />
       </div>
-      <div className="app__body"></div>
+      <div className="app__body">
+        <div className="app__container">
+          <NewsFeed />
+          <Stats />
+        </div>
+      </div>
     </div>
   );
 }
